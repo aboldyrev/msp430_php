04466f113b4313e754210ab7f8071cf62311698e
aboldyrev <andreybold@bk.ru>
2017-01-07 01:39:04 +0300

    Подготовил роут и представление для вывода данных

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', function () {
+	$period = [
+			\Carbon\Carbon::now()->subDay(),
+			\Carbon\Carbon::now()
+	];
+
+	$temperatures = \App\Models\Temperature::whereBetween('created_at', $period)->get();
+	$lights = \App\Models\Light::whereBetween('created_at', $period)->get();
+
+    return view('index', compact('temperatures', 'lights'));
+});
+
+Route::get('welcome', function () {
     return view('welcome');
 });
---
+++ resources/views/index.blade.php
@@ -0,0 +1,1 @@
+@php(dd($temperatures->toArray(), $lights->toArray()))
