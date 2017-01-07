547ba156a02bbe9ecc092350802077e1691f4a19
boldyrev.a <boldyrev.a@studio2i.ru>
2017-01-07 13:44:44 +0300

    Добавил расписание для сбора данных

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+	    $schedule->command('read:serial')->everyMinute();
         // $schedule->command('inspire')
         //          ->hourly();
     }
