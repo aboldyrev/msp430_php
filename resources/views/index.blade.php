7ea5ddb41f54c7fdb0eb30ed9d2f7a7c170d4f87
boldyrev.a <boldyrev.a@studio2i.ru>
2017-01-07 16:11:23 +0300

    Обновил представление для отображения графика

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,17 @@
-@php(dd($temperatures->toArray(), $lights->toArray()))
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Laravel</title>
+		<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	</head>
+	<body>
+		<div id="chart_div"></div>
+
+		<script type="text/javascript">
+			var timestamps = [{!! implode(',', $timestamps) !!}],
+				temperatures = [{!! implode(',', $temperatures) !!}],
+				lights = [{!! implode(',', $lights) !!}];
+		</script>
+		<script type="text/javascript" src="{{ asset('js/chart.js') }}"></script>
+	</body>
+</html>
