99ae35256be11ba61171397242281f146d727858
boldyrev.a <boldyrev.a@studio2i.ru>
2017-01-07 16:09:57 +0300

    Изменил логику сбора данных для отправки в представление

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,17 +1,36 @@
 <?php
 
-Route::get('/', function () {
+Route::get('/', function() {
 	$period = [
-			\Carbon\Carbon::now()->subDay(),
-			\Carbon\Carbon::now()
+		\Carbon\Carbon::now()->subDay(),
+		\Carbon\Carbon::now()
 	];
 
-	$temperatures = \App\Models\Temperature::whereBetween('created_at', $period)->get();
-	$lights = \App\Models\Light::whereBetween('created_at', $period)->get();
+	$temperature_models = \App\Models\Temperature::whereBetween('created_at', $period)->get();
+	$light_models = \App\Models\Light::whereBetween('created_at', $period)->get();
 
-    return view('index', compact('temperatures', 'lights'));
+	$temperatures = [];
+	$lights = [];
+	$timestamps = [];
+
+	foreach ($temperature_models as $key => $temperature_model) {
+		// Сбор временных меток
+		$timestamp = \Carbon\Carbon::parse($temperature_model->created_at);
+		$timestamps[] = '"' .
+			($timestamp->hour < 10 ? '0' . $timestamp->hour : $timestamp->hour) .
+			':' .
+			($timestamp->minute < 10 ? '0' . $timestamp->minute : $timestamp->minute) . '"';
+
+		// Сбор температуры
+		$temperatures[] = '"' . $temperature_model->value . '"';
+
+		// Сбор освещённости
+		$lights[] = '"' . $light_models[$key]->value . '"';
+	}
+
+	return view('index', compact('temperatures', 'lights', 'timestamps'));
 });
 
-Route::get('welcome', function () {
-    return view('welcome');
+Route::get('welcome', function() {
+	return view('welcome');
 });
