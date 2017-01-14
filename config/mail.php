efbfd953e0f36f6486b009d78e7e585ece802802
boldyrev.a <boldyrev.a@studio2i.ru>
2017-01-14 17:09:40 +0300

    Поправил конфиг почты

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('MAIL_TO'), 'name' => env('MAIL_TO_NAME')],
 
     /*
     |--------------------------------------------------------------------------
